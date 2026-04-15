22c8f6c9c6f550f7ec27feb7501d06deec34e991
unknown <shivangirks18@gmail.com>
2026-04-15 09:33:49 +0530

    Corrected JWT

--- src/main/java/com/movie/ticket/platform/utility/JwtUtil.java
+++ src/main/java/com/movie/ticket/platform/utility/JwtUtil.java
@@ -1,8 +1,12 @@
 package com.movie.ticket.platform.utility;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -11,11 +15,17 @@ public class JwtUtil {
     @Value("${jwt.secret}")
     private String secret;
 
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .signWith(getSigningKey())
                 .compact();
     }
 }
+
+
